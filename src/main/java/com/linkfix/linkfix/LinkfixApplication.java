9f7a06174adbc4d559c6cc1dd8e2eb492da44a1c
CyberColchon <coolchonero@gmail.com>
2025-05-13 21:34:33 -0500

    Springboot - Proyecto base LinkFix

---
+++ src/main/java/com/linkfix/linkfix/LinkfixApplication.java
@@ -0,0 +1,13 @@
+package com.linkfix.linkfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkfixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkfix/linkfix/LinkfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkfix.linkfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
